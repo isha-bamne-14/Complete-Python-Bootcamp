8f5d7f069f28ab356bd5deac2477311dd61bdc19
isha-bamne-14 <ishaabamne1@gmail.com>
2025-04-18 17:26:28 +0530

    pushed all the changes

--- 16-Multithreading and Multiprocessing/advance_multi_processing.py
+++ 16-Multithreading and Multiprocessing/advance_multi_processing.py
@@ -9,9 +9,13 @@ def square_number(number):
 
 numbers=[1,2,3,4,5,6,7,8,9,11,2,3,12,14]
 if __name__=="__main__":
+    start_time = time.time() 
 
     with ProcessPoolExecutor(max_workers=3) as executor:
         results=executor.map(square_number,numbers)
 
     for result in results:
         print(result)
+
+    end_time = time.time()  # Record the end time
+    print(f"Total execution time: {end_time - start_time:.2f} seconds")
